a5780b5aef1dee1c78fca62d22ab57fea62fd1c8
Jonas Hansen <jonash871j@gmail.com>
2022-01-25 14:43:37 +0100

    Made improvements

--- Server/Program.cs
+++ Server/Program.cs
@@ -9,13 +9,15 @@ class Program
     {
         AppDomain.CurrentDomain.ProcessExit += OnProcessExit;
         webServer.ServerInfo += OnServerInfo;
+        webServer.Content = File.ReadAllText("index.html"); // The data to be shown in the browser
         webServer.Start();
+
+        // Try it out in the browser http://127.0.0.1:81/
     }
 
     private static void OnProcessExit(object? sender, EventArgs e)
     {
         webServer.Stop();
-        Thread.Sleep(500);
     }
 
     private static void OnServerInfo(string message)
--- WebServerLib/WebServer.cs
+++ WebServerLib/WebServer.cs
@@ -1,59 +1,46 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
+﻿using System.Net;
 using System.Net.Sockets;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace WebServerLib
 {
-    // State object for reading client data asynchronously  
-    public class StateObject
-    {
-        // Size of receive buffer.  
-        public const int BufferSize = 1024;
-
-        // Receive buffer.  
-        public byte[] buffer = new byte[BufferSize];
-
-        // Received data string.
-        public StringBuilder sb = new StringBuilder();
-
-        // Client socket.
-        public Socket workSocket = null;
-    }
-
     public class WebServer
     {
-        public event ServerInfoEvent ServerInfo;
+        public event ServerInfoEvent? ServerInfo;
 
         private readonly Socket serverSocket;
         private readonly Thread webServerThread;
         private readonly ManualResetEvent allDone;
+        private bool isRunning;
 
         public WebServer(IPAddress ipAddress, int port)
         {
+            IpAddress = ipAddress;
+            Port = port;
             allDone = new ManualResetEvent(false);
             serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
             webServerThread = new(WebServerEntry);
-            IpAddress = ipAddress;
-            Port = port;
         }
 
         public IPAddress IpAddress { get; }
         public int Port { get; }
-        public bool IsRunning { get; private set; }
+        public string Content { get; set; } = string.Empty;
 
+        /// <summary>
+        /// Used to start the server
+        /// </summary>
         public void Start()
         {
             try
             {
+                // Bind ip and port to server socket
                 serverSocket.Bind(new IPEndPoint(IpAddress, Port));
+
+                // Listen up to 10 clients
                 serverSocket.Listen(10);
-                serverSocket.ReceiveTimeout = 1000;
-                serverSocket.SendTimeout = 1000;
-                IsRunning = true;
+
+                // Start web server thread
+                isRunning = true;
                 webServerThread.Start();
             }
             catch (Exception ex)
@@ -62,74 +49,82 @@ namespace WebServerLib
             }
         }
 
+        /// <summary>
+        /// Used to stop the server
+        /// </summary>
         public void Stop()
         {
-            try
+            if (isRunning)
             {
-                if (IsRunning)
-                {
-                    ServerInfo?.Invoke("Server is closing...");
-                    IsRunning = false;
-                    webServerThread.Join();
-                    serverSocket.Close();
-                    ServerInfo?.Invoke("Server stopped successfully");
-                }
-            }
-            catch (Exception ex)
-            {
-                ServerInfo?.Invoke("Server failed to stop: " + ex.Message);
+                ServerInfo?.Invoke("Server is closing...");
+
+                // Joins thread and closes socket
+                isRunning = false;
+                webServerThread.Join();
+                serverSocket.Close();
             }
         }
 
+        /// <summary>
+        /// The entry point for the server
+        /// </summary>
         private void WebServerEntry()
         {
-            ServerInfo?.Invoke("Server started successfully");
-           
-            while (IsRunning)
+            ServerInfo?.Invoke($"Server started on {IpAddress.MapToIPv4()}:{Port}");
+
+            while (isRunning)
             {
-                allDone.Reset();
+                // Begin accepting a new client
                 serverSocket.BeginAccept(new AsyncCallback(AcceptCallback), serverSocket);
+
+                // Waits until a client tries to connect
                 allDone.WaitOne();
+                allDone.Reset();
             }
         }
 
+        /// <summary>
+        /// The entry point for a client request
+        /// </summary>
         private void AcceptCallback(IAsyncResult ar)
         {
+            // The clients has tried to connect continue in server thread 
             allDone.Set();
-            Socket? listener = (Socket?)ar.AsyncState;
-            if (listener == null)
-            {
-                return;
-            }
-            Socket clientSocket = listener.EndAccept(ar);
-            ServerInfo?.Invoke($"A request was made from a client");
 
-            byte[] buffer = new byte[10240]; // 10 kb, just in case
-            int receivedBCount = clientSocket.Receive(buffer); // Receive the request
-            string strReceived = Encoding.UTF8.GetString(buffer, 0, receivedBCount);
-            ServerInfo?.Invoke($"Data amount recived from client: {strReceived.Length} ");
-            SendResponse(clientSocket, File.ReadAllText("index.html"), "200 OK", "text/html");
+            Socket clientSocket = ((Socket)ar.AsyncState).EndAccept(ar);
+            HandleRequest(clientSocket);
+            SendResponse(clientSocket, Content);
         }
 
-        private void SendResponse(Socket clientSocket, string bContent, string responseCode, string contentType)
+        /// <summary>
+        /// Used to handle request from a client
+        /// </summary>
+        private void HandleRequest(Socket clientSocket)
         {
-            try
-            {
-                byte[] bHeader = Encoding.UTF8.GetBytes(
-                                    "HTTP/1.1 " + responseCode + "\r\n"
-                                  + "Server: Atasoy Simple Web Server\r\n"
-                                  + "Content-Length: " + bContent.Length.ToString() + "\r\n"
-                                  + "Connection: close\r\n"
-                                  + "Content-Type: " + contentType + "\r\n\r\n");
-                clientSocket.Send(bHeader);
-                clientSocket.Send(Encoding.UTF8.GetBytes(bContent));
-                clientSocket.Close();
-                ServerInfo?.Invoke("Server responded to client client successfully");
-            }
-            catch (Exception ex)
-            {
-                ServerInfo?.Invoke($"Failed to respond to client: {ex.Message}");
-            }
+            byte[] buffer = new byte[10240];
+            int receivedByteAmount = clientSocket.Receive(buffer);
+            ServerInfo?.Invoke($"Data amount recived from client: {receivedByteAmount} ");
+        }
+
+        /// <summary>
+        /// Used to send response to a client
+        /// </summary>
+        private void SendResponse(Socket clientSocket, string content)
+        {
+            // Http header
+            byte[] header = Encoding.UTF8.GetBytes(
+                  "HTTP/1.1 200 OK\r\n"
+                + "Server: Atasoy Simple Web Server\r\n"
+                + "Content-Length: " + content.Length.ToString() + "\r\n"
+                + "Connection: close\r\n"
+                + "Content-Type: text/html\r\n\r\n");
+
+            // Send data and close the client connection
+            clientSocket.Send(header);
+            clientSocket.Send(Encoding.UTF8.GetBytes(content));
+            clientSocket.Close();
+
+            ServerInfo?.Invoke("Server responded to client client successfully");
         }
     }
 }
