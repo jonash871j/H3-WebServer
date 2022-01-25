bdc18fd678420873be9f40d3cadbad16c90d6bba
Jonas Hansen <jonash871j@gmail.com>
2022-01-25 14:44:30 +0100

    Minor change

--- WebServerLib/Delegates.cs
+++ WebServerLib/Delegates.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace WebServerLib
+﻿namespace WebServerLib
 {
     public delegate void ServerInfoEvent(string message);
 }
